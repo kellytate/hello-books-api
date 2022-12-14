11ce43c223b8f7721e19553c552576604039a7f2
kelly <kelly.tate.dev@gmail.com>
2022-12-13 20:10:43 -0500

    Added Book class and books endpoint

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@
 from flask import Blueprint
 
 hello_world_bp = Blueprint("hello_world_bp", __name__)
+books_bp = Blueprint("books", __name__, url_prefix="/books")
 
 
 @hello_world_bp.route("/hello-world", methods=["GET"])
@@ -28,3 +29,16 @@ def broken_endpoint():
     new_hobby = "Surfing"
     response_body["hobbies"].append(new_hobby)
     return response_body
+
+
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
+
+books = [
+    Book(1, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
+    Book(2, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
+    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
+] 
