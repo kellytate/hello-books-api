740a211151a6c8f8a8fa3359b3e0a6bae7c05a65
kelly <kelly.tate.dev@gmail.com>
2023-01-03 19:22:07 -0800

    Added tests for models and Added to_dict

--- app/models/book.py
+++ app/models/book.py
@@ -4,3 +4,17 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
+
+    def to_dict(self):
+        # Create an empty dictionary
+        # for each attribute in Book
+        # create a key with the attribute's name
+        # set it to the value of the attribute
+        # return the dictionary
+
+        book_dict = {}
+        book_dict["id"] = self.id
+        book_dict["title"] = self.title
+        book_dict["description"] = self.description
+        
+        return book_dict
